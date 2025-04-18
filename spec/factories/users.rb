f4e635bd17121c2c01eb12950ed8b66d3f52a059
atenente <anderson.tenente1@gmail.com>
2025-04-18 13:34:51 -0300

    Test: adiciona associação com posts

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :user do
-    login { Faker::Internet.username }
+    login { Faker::Internet.unique.username }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,23 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validations" do
-    it "is valid with valid attributes" do
+    it "é valido com login" do
       user = build(:user)
       expect(user).to be_valid
     end
 
-    it "is invalid without login" do
+    it "é invalido sem login" do
       user = build(:user, login: nil)
       expect(user).not_to be_valid
       expect(user.errors[:login]).to include("can't be blank")
+    end
+  end
+
+  describe "associations" do
+    it "has many posts" do
+      user = create(:user)
+      3.times { create(:post, user: user) }
+      expect(user.posts.count).to eq(3)
     end
   end
 end
