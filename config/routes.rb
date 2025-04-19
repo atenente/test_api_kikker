35b8d4afd6e509acfd3363bcb45f196a4cfb373e
atenente <anderson.tenente1@gmail.com>
2025-04-18 23:42:08 -0300

    Add: model and controller posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :posts, only: [:create, :index]
+      resources :posts, only: [:create, :index] do
+        collection do
+          get :top_posts
+          get :authors_ips
+        end
+      end
+      resources :ratings, only: [:create]
     end
   end
 end
