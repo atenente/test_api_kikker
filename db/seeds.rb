1e4da1a8ff5febc4c663a4e8358f6ba9899e3546
atenente <anderson.tenente1@gmail.com>
2025-04-19 00:19:32 -0300

    Config: adiciona registros ao banco para testes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,65 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+require 'json'
+require 'httparty'
+
+# Quantidade
+TOTAL_USERS = 100
+TOTAL_POSTS = 200_000
+TOTAL_RATINGS = (TOTAL_POSTS * 0.75).to_i
+UNIQUE_IPS = Array.new(50) { Faker::Internet.unique.ip_v4_address }
+
+# Limpeza dos bancos antes de começar
+Rails.logger.debug "Limpando bancos de dados..."
+Rating.delete_all
+Post.delete_all
+User.delete_all
+Rails.logger.debug "Bancos limpos com sucesso!"
+
+# Criação de posts
+Rails.logger.debug "Gerando posts..."
+TOTAL_POSTS.times do |_i|
+  login = Faker::Internet.unique.username
+  rand(1..TOTAL_USERS)
+  title = 'teste de titulo'
+  body = 'teste de texto não utilizei faker para o processamentos ser mais rápido'
+  ip = UNIQUE_IPS.sample
+
+  payload = {
+    user: { login: login },
+    post: {
+      title: title,
+      body: body,
+      ip: ip
+    }
+  }
+
+  HTTParty.post('http://localhost:3000/api/v1/posts',
+                body: payload.to_json,
+                headers: { 'Content-Type' => 'application/json' })
+end
+
+# Criação de ratings
+Rails.logger.debug "Gerando ratings..."
+TOTAL_RATINGS.times do
+  used_votes = {}
+  post_id = rand(1..TOTAL_POSTS)
+  user_id = rand(1..TOTAL_USERS)
+  key = "#{user_id}-#{post_id}"
+
+  next if used_votes[key] # garante um voto por usuário por post
+
+  used_votes[key] = true
+  value = rand(1..5)
+
+  payload = {
+    rating: {
+      post_id: post_id,
+      user_id: user_id,
+      value: value
+    }
+  }
+
+  HTTParty.post('http://localhost:3000/api/v1/posts',
+                body: payload.to_json,
+                headers: { 'Content-Type' => 'application/json' })
+end
